1fa1d52a3db6604c02863a5204b6b9775cfa69da
Anan Wang <mengan.wang@gmail.com>
2025-01-17 21:13:50 -0800

    added some tests to verify firebase

--- app/utils/firebase.py
+++ app/utils/firebase.py
@@ -1,8 +1,10 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
+from dotenv import load_dotenv
 
 # Initialize Firebase Admin with your service account
-cred = credentials.Certificate("../../serviceAccountKey.json")
+cred = credentials.Certificate("env/databridge-c1802-firebase-adminsdk-9gliq-535eb02d20.json")
 firebase_admin.initialize_app(cred)
 
 # Get Firestore client

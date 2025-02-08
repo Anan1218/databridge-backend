3af45e1da375052dc1364da9636c98417ab1c4cf
Mengan Wang <mengan.wang@gmail.com>
2025-02-08 00:39:09 -0800

    restart from scratch

--- run_scheduler.py
+++
@@ -1,6 +0,0 @@
-from app.utils.search_scheduler import run_scheduler
-
-if __name__ == "__main__":
-    print("Search scheduler is temporarily disabled")
-    # run_scheduler()
-    print("Search scheduler completed.") 
